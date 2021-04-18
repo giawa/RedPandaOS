c3a0956a83254cd59ce4cc6ed43f93765ba5013b
giawa <giawa@hotmail.com>
2021-04-17 22:56:47 -0700

    Use the new metadata that was just processed to find the Program.Main rvaOffset to get an entry point into the program instead of using a hardcoded rva.

--- ILInterpreter/Program.cs
+++ ILInterpreter/Program.cs
@@ -9,15 +9,38 @@ namespace ILInterpreter
         {
             PortableExecutableFile file = new PortableExecutableFile(@"..\..\..\..\TestIL\bin\Debug\netcoreapp3.1\TestIL.dll");
 
-            uint rvaOffset = 0x2050;
-            MethodHeader method = new MethodHeader(file.Memory, ref rvaOffset);
+            // find the Program.Main entry point
+            uint rvaOffset = FindEntryPoint(file, "Program", "Main");
 
-            Interpreter interpreter = new Interpreter();
-            interpreter.LoadMethod(file.Metadata, method);
-            interpreter.Execute();
+            if (rvaOffset != 0)
+            {
+                MethodHeader method = new MethodHeader(file.Memory, ref rvaOffset);
 
-            Console.WriteLine($"Interpreted method returned: {interpreter.ReturnValue}");
+                Interpreter interpreter = new Interpreter();
+                interpreter.LoadMethod(file.Metadata, method);
+                interpreter.Execute();
+
+                Console.WriteLine($"Interpreted method returned: {interpreter.ReturnValue}");
+            }
+            
             Console.ReadKey();
+        }
+
+        private static uint FindEntryPoint(PortableExecutableFile file, string typeName, string methodName)
+        {
+            foreach (var typeDef in file.Metadata.TypeDefs)
+            {
+                if (typeDef.GetName(file.Metadata) == "Program")
+                {
+                    foreach (var methodDef in typeDef.Methods)
+                    {
+                        if (methodDef.GetName(file.Metadata) == "Main")
+                            return methodDef.rva;
+                    }
+                }
+            }
+
+            return 0;
         }
     }
 }
