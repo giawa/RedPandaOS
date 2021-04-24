390e382dabcff755de6ed93bc8107904d99295de
giawa <giawa@hotmail.com>
2021-04-23 21:51:14 -0700

    The first non-trivial program that can be compiled (in debug mode only, and ymmv depending on your compiler settings/etc).  This will calculate and display all primes below 80 as hex numbers.

--- TestIL/Program.cs
+++ TestIL/Program.cs
@@ -1,35 +1,124 @@
-﻿using System;
+﻿using IL2Asm;
 
 namespace TestIL
 {
     class Program
     {
-        static int Main(string[] args)
+        private const string _welcomeMessage = "Welcome to C#!";
+
+        [BootEntryPoint]
+        [RealMode]
+        static void Main()
         {
-            for (int check = 1; check < 10000; check++)
+            for (int i = 1; i < 80; i++)
             {
-                bool isPrime = true;
-
-                if (check <= 1) isPrime = false;
-                else if (check == 2) isPrime = true;
-                else if ((check % 2) == 0) isPrime = false;
-                else
+                int prime = IsPrime(i);
+                if (prime != 0)
                 {
-                    for (int i = 3; i < check / 2; i += 2)
-                    {
-                        if ((check % i) == 0) isPrime = false;
-                    }
-                }
-
-                if (isPrime)
-                {
-                    Console.Write(check.ToString());
-                    Console.WriteLine(" is Prime!");
+                    WriteHex(prime);
+                    Bios.WriteByte('\n');
+                    Bios.WriteByte('\r');
                 }
             }
 
-            //Console.ReadKey();
-            return 1;
+            while (true) ;
+        }
+
+        [RealMode]
+        public static int IsPrime(int num)
+        {
+            if (num == 1) return 0;
+            else
+            {
+                for (int i = 2; i < num; i++)
+                {
+                    if (Modulo(num, i) == 0) return 0;
+                }
+                return num;
+            }
+        }
+
+        [RealMode]
+        public static int Modulo(int source, int div)
+        {
+            int remainder = source;
+            while (remainder >= div) remainder -= div;
+            return remainder;
+        }
+
+        [RealMode]
+        public static void WriteHex(int value)
+        {
+            WriteHexChar(value >> 4);
+            WriteHexChar(value & 0x0f);
+        }
+
+        [RealMode]
+        public static void WriteHexChar(int value)
+        {
+            if (value >= 10) Bios.WriteByte(value + 55);
+            else Bios.WriteByte(value + 48);
+        }
+
+        [RealMode]
+        public static void WriteLine(string s)
+        {
+            Write(s);
+            Bios.WriteByte((byte)'\n');
+            Bios.WriteByte((byte)'\r');
+        }
+
+        [RealMode]
+        public static void Write(string s)
+        {
+            int l = s.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                Bios.WriteByte((byte)s[i]);
+            }
+        }
+
+        [RealMode]
+        public static void WriteInt(int value)
+        {
+            int tens = 0;
+            int scratch = value;
+
+            while (scratch > 0)
+            {
+                tens++;
+                scratch = Divide(scratch, 10);
+            }
+
+            while (tens > 0)
+            {
+                scratch = 1;
+                for (int i = 0; i < tens - 1; i++) scratch *= 10;
+
+                int c = Divide(value, scratch);
+                c = Modulo(c, 10);
+                c += 48;
+
+                Bios.WriteByte((byte)c);
+
+                tens--;
+            }
+        }
+
+        [RealMode]
+        public static int Divide(int source, int div)
+        {
+            int remainder = source;
+            int quotient = 0;
+
+            while (remainder >= div)
+            {
+                remainder -= div;
+                quotient++;
+            }
+
+            return quotient;
         }
     }
 }
