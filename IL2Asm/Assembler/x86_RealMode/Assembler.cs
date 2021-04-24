87f226999a12c13f2ddc3ccc14cf0dfa084fa80d
giawa <giawa@hotmail.com>
2021-04-23 21:44:50 -0700

    Add support for several more IL opcodes and add basic support for returning a value via ax.
    Fix up some bugs that existed because I forgot the instruction pointer was pushed on to the stack when calling functions.

--- IL2Asm/Assembler/x86_RealMode/Assembler.cs
+++ IL2Asm/Assembler/x86_RealMode/Assembler.cs
@@ -10,12 +10,13 @@ namespace IL2Asm.Assembler.x86_RealMode
     {
         private List<AssembledMethod> _methods = new List<AssembledMethod>();
         private Dictionary<string, object> _initializedData = new Dictionary<string, object>();
-        private object[] _localVariables = new object[256];
 
-        private int _bytesPerRegister = 2;
+        public const int BytesPerRegister = 2;
 
         private sbyte _sbyte;
+        private byte _byte;
         private string _jmpLabel;
+        private uint _uint;
 
         public void Assemble(VirtualMemory memory, CLIMetadata metadata, MethodDefLayout methodDef)
         {
@@ -26,6 +27,7 @@ namespace IL2Asm.Assembler.x86_RealMode
 
             if (_methods.Count == 0)
             {
+                assembly.AddAsm("[bits 16]");    // for bootsector code only
                 assembly.AddAsm("[org 0x7c00]");    // for bootsector code only
                 assembly.AddAsm("");
             }
@@ -49,11 +51,8 @@ namespace IL2Asm.Assembler.x86_RealMode
                         assembly.AddAsm("push dx");
                         assembly.AddAsm("mov dx, 0");
                     }
-                    if (localVarCount > 2)
-                    {
+                    for (int i = 2; i < localVarCount; i++)
                         assembly.AddAsm("push 0");
-                    }
-                    if (localVarCount > 3) throw new Exception("Too many local variables for now, need a heap.");
                 }
             }
 
@@ -70,11 +69,31 @@ namespace IL2Asm.Assembler.x86_RealMode
 
                 switch (opcode)
                 {
-                    case 0x00: assembly.AddAsm("nop"); break;   // NOP
+                    case 0x00: /*assembly.AddAsm("nop");*/ break;   // NOP
 
                     // LDARG.0
                     case 0x02:
-                        assembly.AddAsm($"push [bp + {_bytesPerRegister * 2}]");
+                        _uint = method.MethodDef.MethodSignature.ParamCount - 0;
+                        assembly.AddAsm($"mov ax, [bp + {BytesPerRegister * (1 + _uint)}]");
+                        assembly.AddAsm("push ax");
+                        break;
+                    // LDARG.1
+                    case 0x03:
+                        _uint = method.MethodDef.MethodSignature.ParamCount - 1;
+                        assembly.AddAsm($"mov ax, [bp + {BytesPerRegister * (1 + _uint)}]");
+                        assembly.AddAsm("push ax");
+                        break;
+                    // LDARG.2
+                    case 0x04:
+                        _uint = method.MethodDef.MethodSignature.ParamCount - 2;
+                        assembly.AddAsm($"mov ax, [bp + {BytesPerRegister * (1 + _uint)}]");
+                        assembly.AddAsm("push ax");
+                        break;
+                    // LDARG.3
+                    case 0x05:
+                        _uint = method.MethodDef.MethodSignature.ParamCount - 3;
+                        assembly.AddAsm($"mov ax, [bp + {BytesPerRegister * (1 + _uint)}]");
+                        assembly.AddAsm("push ax");
                         break;
 
                     // LDLOC.0
@@ -83,12 +102,29 @@ namespace IL2Asm.Assembler.x86_RealMode
                         break;
                     // LDLOC.1
                     case 0x07:
-                        assembly.AddAsm("push cx");
+                        assembly.AddAsm("push dx");
                         break;
                     // LDLOC.2
                     case 0x08:
-                        assembly.AddAsm("mov ax, [bp]");
+                        assembly.AddAsm("mov ax, [bp - 6]");
                         assembly.AddAsm("push ax");
+                        break;
+                    // LDLOC.3
+                    case 0x09:
+                        assembly.AddAsm("mov ax, [bp - 8]");
+                        assembly.AddAsm("push ax");
+                        break;
+
+                    // STLOC.S
+                    case 0x13:
+                        _byte = code[i++];
+                        if (_byte == 0) assembly.AddAsm("pop cx");
+                        else if (_byte == 1) assembly.AddAsm("pop dx");
+                        else
+                        {
+                            assembly.AddAsm("pop ax");
+                            assembly.AddAsm($"mov [bp - {BytesPerRegister * (_byte + 1)}], ax");
+                        }
                         break;
 
                     // LDC.I4.0
@@ -104,61 +140,149 @@ namespace IL2Asm.Assembler.x86_RealMode
                     case 0x0B:
                         assembly.AddAsm("pop dx");
                         break;
-                    // STLOC.1
+                    // STLOC.2
                     case 0x0C:
                         assembly.AddAsm("pop ax");
-                        assembly.AddAsm("mov [bp], ax");
+                        assembly.AddAsm("mov [bp - 6], ax");
                         break;
+                    // STLOC.3
+                    case 0x0D:
+                        assembly.AddAsm("pop ax");
+                        assembly.AddAsm("mov [bp - 8], ax");
+                        break;
+
+                    // LDLOC.S
+                    case 0x11:
+                        _byte = code[i++];
+                        if (_byte == 0) assembly.AddAsm("push cx");
+                        else if (_byte == 1) assembly.AddAsm("push dx");
+                        else
+                        {
+                            assembly.AddAsm($"mov ax, [bp - {BytesPerRegister * (_byte + 1)}]");
+                            assembly.AddAsm("push ax");
+                        }
+                        break;
+
+                    // LDC.I4.1
+                    case 0x17: assembly.AddAsm("push 1"); break;
+                    case 0x18: assembly.AddAsm("push 2"); break;
+                    case 0x19: assembly.AddAsm("push 3"); break;
+                    case 0x1A: assembly.AddAsm("push 4"); break;
+                    case 0x1B: assembly.AddAsm("push 5"); break;
+                    case 0x1C: assembly.AddAsm("push 6"); break;
+                    case 0x1D: assembly.AddAsm("push 7"); break;
+                    case 0x1E: assembly.AddAsm("push 8"); break;
 
                     // LDC.I4.S
                     case 0x1F:
                         assembly.AddAsm($"push {code[i++]}");
                         break;
-                    
-                    // LDC.I4.1
-                    case 0x17:
-                        assembly.AddAsm("push 1");
+
+                    // LDC.I4
+                    case 0x20:
+                        int value = BitConverter.ToInt32(code, i);
+                        if (value > short.MaxValue || value < short.MinValue) 
+                            throw new Exception("Out of range for 16 bit mode");
+                        i += 4;
+                        assembly.AddAsm($"push {value}");
+                        break;
+
+                    // POP
+                    case 0x26:
+                        assembly.AddAsm("pop ax");
                         break;
 
                     case 0x28: CALL(assembly, metadata, code, ref i); break;
                     case 0x6F: CALLVIRT(assembly, metadata, code, ref i); break;
 
                     // RET
-                    case 0x002A:
+                    case 0x2A:
+                        // place the returned value on ax, which should clear our CLI stack
+                        if (method.MethodDef.MethodSignature.RetType != null && method.MethodDef.MethodSignature.RetType.Type != ElementType.EType.Void)
+                        {
+                            assembly.AddAsm("pop ax; return value");
+                        }
+
+                        // pop any local variables we pushed at the start
                         if (method.LocalVars != null)
                         {
                             int localVarCount = method.LocalVars.LocalVariables.Length;
-                            if (localVarCount > 2) assembly.AddAsm("pop");      // this was actually just a 0
-                            if (localVarCount > 1) assembly.AddAsm("pop dx");
-                            if (localVarCount > 0) assembly.AddAsm("pop cx");
+                            for (int p = 2; p < localVarCount; p++)
+                                assembly.AddAsm("pop bx; localvar that was pushed on stack");
+                            if (localVarCount > 1) assembly.AddAsm("pop dx; localvar.1");
+                            if (localVarCount > 0) assembly.AddAsm("pop cx; localvar.0");
                         }
 
-                        int bytes = (int)methodDef.paramList * _bytesPerRegister;
+                        int bytes = (int)methodDef.MethodSignature.ParamCount * BytesPerRegister;
                         assembly.Assembly.Add("pop bp");
                         assembly.Assembly.Add($"ret {bytes}");
                         break;
 
                     // BR.S
-                    case 0x002B:
+                    case 0x2B:
                         _sbyte = (sbyte)code[i++];
                         _jmpLabel = $"IL_{(i + _sbyte).ToString("X4")}_{_methods.Count}";
                         assembly.AddAsm($"jmp {_jmpLabel}");
                         break;
 
-                    // BRTRUE.S
-                    case 0x002D:
+                    // BRFALSE.S
+                    case 0x2C:
                         _sbyte = (sbyte)code[i++];
                         _jmpLabel = $"IL_{(i + _sbyte).ToString("X4")}_{_methods.Count}";
                         assembly.AddAsm("pop ax");
-                        assembly.AddAsm("cmp ax,1");
+                        assembly.AddAsm("cmp ax, 0");
                         assembly.AddAsm($"je {_jmpLabel}");
                         break;
 
+                    // BRTRUE.S
+                    case 0x2D:
+                        _sbyte = (sbyte)code[i++];
+                        _jmpLabel = $"IL_{(i + _sbyte).ToString("X4")}_{_methods.Count}";
+                        assembly.AddAsm("pop ax");
+                        assembly.AddAsm("cmp ax, 1");
+                        assembly.AddAsm($"je {_jmpLabel}");
+                        break;
+
+                    // ADD
                     case 0x58:
-                        assembly.Assembly.Add("pop ax");
                         assembly.Assembly.Add("pop bx");
+                        assembly.Assembly.Add("pop ax");
                         assembly.Assembly.Add("add ax, bx");
                         assembly.Assembly.Add("push ax");
+                        break;
+
+                    // SUB
+                    case 0x59:
+                        assembly.Assembly.Add("pop bx");
+                        assembly.Assembly.Add("pop ax");
+                        assembly.Assembly.Add("sub ax, bx");
+                        assembly.Assembly.Add("push ax");
+                        break;
+
+                    // MUL
+                    case 0x5A:
+                        assembly.Assembly.Add("pop bx");
+                        assembly.Assembly.Add("pop ax");
+                        assembly.Assembly.Add("mul bx");
+                        assembly.Assembly.Add("push ax");
+                        break;
+
+                    // AND
+                    case 0x5F:
+                        assembly.AddAsm("pop bx");
+                        assembly.AddAsm("pop ax");
+                        assembly.AddAsm("and ax, bx");
+                        assembly.AddAsm("push ax");
+                        break;
+
+                    // SHR
+                    case 0x63:
+                        assembly.AddAsm("mov bx, cx");  // cx is needed to access cl for variable shift, so store is in bx temporarily
+                        assembly.AddAsm("pop cx");      // get amount to shift
+                        assembly.AddAsm("pop ax");
+                        assembly.AddAsm("shr ax, cl");
+                        assembly.AddAsm("mov cx, bx");  // restore cx
+                        assembly.AddAsm("push ax");
                         break;
 
                     case 0x72: LDSTR(assembly, metadata, code, ref i); break;// LDSTR
@@ -178,25 +302,58 @@ namespace IL2Asm.Assembler.x86_RealMode
                         assembly.Assembly.Add("push ax");
                         break;
 
-                    // CLT
-                    case 0xFE04:
-                        assembly.Assembly.Add("pop ax");           // value2
-                        assembly.Assembly.Add("pop bx");           // value1
-                        assembly.Assembly.Add("cmp ax, bx"); // compare values
-                        assembly.Assembly.Add("lahf");              // load flags into ah
-                        assembly.Assembly.Add("and ah, 128");        // get only the signs info
-                        assembly.Assembly.Add("shr ax, 15");        // push the 1 into the LSB
+                    // CEQ
+                    case 0xFE01:
+                        assembly.Assembly.Add("pop ax");        // value2
+                        assembly.Assembly.Add("pop bx");        // value1
+                        assembly.Assembly.Add("cmp ax, bx");    // compare values
+                        assembly.Assembly.Add("lahf");          // load flags into ah
+                        assembly.Assembly.Add("shr ax, 14");    // push the 1 into the LSB
+                        assembly.Assembly.Add("and ax, 1");     // push the 1 into the LSB
                         assembly.Assembly.Add("push ax");
                         break;
 
-                    default: throw new Exception($"Unknown IL opcode 0x{opcode.ToString("X")}");
+                    // CGT
+                    case 0xFE02:
+                        assembly.Assembly.Add("pop ax");        // value2
+                        assembly.Assembly.Add("pop bx");        // value1
+                        assembly.Assembly.Add("cmp ax, bx");    // compare values
+                        assembly.Assembly.Add("lahf");          // load flags into ah
+                        assembly.Assembly.Add("shr ax, 15");    // push the 1 into the LSB
+                        assembly.Assembly.Add("and ax, 1");     // push the 1 into the LSB
+                        assembly.Assembly.Add("push ax");
+                        break;
+
+                    // CGT.UN (identical to CGT for now)
+                    case 0xFE03:
+                        assembly.Assembly.Add("pop ax");        // value2
+                        assembly.Assembly.Add("pop bx");        // value1
+                        assembly.Assembly.Add("cmp ax, bx");    // compare values
+                        assembly.Assembly.Add("lahf");          // load flags into ah
+                        assembly.Assembly.Add("shr ax, 15");    // push the 1 into the LSB
+                        assembly.Assembly.Add("and ax, 1");     // push the 1 into the LSB
+                        assembly.Assembly.Add("push ax");
+                        break;
+
+                    // CLT
+                    case 0xFE04:
+                        assembly.Assembly.Add("pop ax");        // value2
+                        assembly.Assembly.Add("pop bx");        // value1
+                        assembly.Assembly.Add("cmp bx, ax");    // compare values
+                        assembly.Assembly.Add("lahf");          // load flags into ah
+                        assembly.Assembly.Add("shr ax, 15");    // push the 1 into the LSB
+                        assembly.Assembly.Add("and ax, 1");     // push the 1 into the LSB
+                        assembly.Assembly.Add("push ax");
+                        break;
+
+                    default: throw new Exception($"Unknown IL opcode 0x{opcode.ToString("X")} at {label}");
                 }
 
                 // remove the label if no new assembly was added
                 if (assembly.Assembly.Count == asmCount)
                 {
                     //assembly.Assembly.RemoveAt(assembly.Assembly.Count - 1);
-                    assembly.Assembly.Add("nop");
+                    //assembly.Assembly.Add("nop");
                 }
             }
 
@@ -284,6 +441,8 @@ namespace IL2Asm.Assembler.x86_RealMode
 
                 string callsite = methodDef.ToString().Replace(".", "_");
                 assembly.AddAsm($"call {callsite}");
+
+                if (methodDef.MethodSignature.RetType != null && methodDef.MethodSignature.RetType.Type != ElementType.EType.Void) assembly.AddAsm("push ax");
             }
             else throw new Exception("Unhandled CALL target");
         }
@@ -301,12 +460,18 @@ namespace IL2Asm.Assembler.x86_RealMode
                 if (memberName == "System.String.get_Chars")
                 {
                     assembly.AddAsm("; System.String.get_Chars plug");
-                    assembly.AddAsm("TODO");
+                    assembly.AddAsm("pop ax");  // pop index
+                    assembly.AddAsm("pop bx");  // pop this
+                    assembly.AddAsm("add ax, bx");
+                    assembly.AddAsm("mov bx, ax");
+                    assembly.AddAsm("mov ax, [bx]");
+                    assembly.AddAsm("push ax");
                 }
                 else if (memberName == "System.String.get_Length")
                 {
                     assembly.AddAsm("; System.String.get_Length plug");
-                    assembly.AddAsm("TODO");
+                    assembly.AddAsm("pop bx");  // pop this
+                    assembly.AddAsm("push 14");
                 }
                 else
                 {
@@ -332,6 +497,7 @@ namespace IL2Asm.Assembler.x86_RealMode
                     stream.WriteLine($"; Exporting assembly for method {method.Method.MethodDef}");
                     foreach (var line in method.Assembly)
                     {
+                        if (!line.EndsWith(":") && !line.StartsWith("[")) stream.Write("    ");
                         stream.WriteLine(line);
 
                         if (line == "call printstring")
@@ -349,18 +515,21 @@ namespace IL2Asm.Assembler.x86_RealMode
                 }
                 stream.WriteLine();*/
 
-                stream.WriteLine("; Exporting initialized data");
-                foreach (var data in _initializedData)
+                if (_initializedData.Count > 0)
                 {
-                    if (data.Value is string)
+                    stream.WriteLine("; Exporting initialized data");
+                    foreach (var data in _initializedData)
                     {
-                        stream.WriteLine($"{data.Key}:");
-                        stream.WriteLine($"    db '{data.Value}', 0");  // 0 for null termination after the string
+                        if (data.Value is string)
+                        {
+                            stream.WriteLine($"{data.Key}:");
+                            stream.WriteLine($"    db '{data.Value}', 0");  // 0 for null termination after the string
+                        }
                     }
+                    stream.WriteLine();
                 }
 
                 // should only do this for boot sector attribute code
-                stream.WriteLine();
                 stream.WriteLine("times 510-($-$$) db 0");
                 stream.WriteLine("dw 0xaa55");
             }
