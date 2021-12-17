d0a6aa3af032f51f9225cdf83d7d7ca4a05e710a
giawa <giawa@hotmail.com>
2021-12-16 22:40:52 -0800

    [Runtime] Add Ceiling, Floor and Divide with remainder to the Math32 library

--- RedPandaOS/Runtime/Math32.cs
+++ RedPandaOS/Runtime/Math32.cs
@@ -2,6 +2,20 @@
 {
     public static class Math32
     {
+        public static int Divide(int source, int div, out int remainder)
+        {
+            remainder = source;
+            int quotient = 0;
+
+            while (remainder >= div)
+            {
+                remainder -= div;
+                quotient++;
+            }
+
+            return quotient;
+        }
+
         public static int Divide(int source, int div)
         {
             int remainder = source;
@@ -28,6 +42,20 @@
             int remainder = source;
             while (remainder >= div) remainder -= div;
             return remainder;
+        }
+
+        public static int Ceiling(int source, int div)
+        {
+            var result = Divide(source, div, out int remainder);
+
+            if (remainder > 0) result++;
+
+            return result;
+        }
+
+        public static int Floor(int source, int div)
+        {
+            return Divide(source, div);
         }
     }
 }
