db910c00bda63112323c9f59d31ec36d912f8060
giawa <giawa@hotmail.com>
2021-12-28 22:54:17 -0800

    [OS] Added a way to (unsafely) cast between objects.  This can be useful for things like converting a uint[] to a byte[], etc.

--- RedPandaOS/Kernel/Memory/Utilities.cs
+++ RedPandaOS/Kernel/Memory/Utilities.cs
@@ -5,6 +5,18 @@ namespace Kernel.Memory
     public static class Utilities
     {
         [AsmMethod]
+        public static T UnsafeCast<T>(object obj)
+        {
+            return default(T);
+        }
+
+        [AsmPlug("Kernel_Memory_Utilities_UnsafeCast_MVar_Object", IL2Asm.BaseTypes.Architecture.X86)]
+        private static void UnsafeCastTAsm(IAssembledMethod assembly)
+        {
+            assembly.AddAsm($"; PtrToObject nop");
+        }
+
+        [AsmMethod]
         public static T PtrToObject<T>(uint addr)
         {
             return default(T);
