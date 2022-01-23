21934e6d63be88c6ca9a03e0da10916e1fbe8519
giawa <giawa@hotmail.com>
2022-01-23 08:44:53 -0800

    [OS] Add code to copy and move a stack.
    Note:  This relies on replacing EBP using a heuristic, and may fail in some cases.

---
+++ RedPandaOS/Kernel/Memory/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kernel.Memory
+{
+    public static class Stack
+    {
+        public static void MoveStack(uint oldStackAddr, uint newStackAddr, uint size)
+        {
+            for (uint i = newStackAddr; i >= newStackAddr - size; i -= 4096)
+            {
+                Paging.AllocateFrame(Paging.GetPage(i, true, Paging.CurrentDirectory), false, true);
+            }
+
+            Paging.FlushTLB();
+
+            var esp = CPUHelper.CPU.ReadESP();
+            var ebp = CPUHelper.CPU.ReadEBP();
+
+            var offset = newStackAddr - oldStackAddr;
+            uint newEsp = esp + offset;
+            uint newEbp = ebp + offset;
+
+            CPUHelper.CPU.FastCopyDWords(esp, newEsp, oldStackAddr - esp);
+
+            for (uint i = newStackAddr; i > newEsp; i -= 4)
+            {
+                var value = CPUHelper.CPU.ReadMemInt(i);
+
+                if (value < oldStackAddr && value >= esp)
+                {
+                    value = value + offset;
+                    CPUHelper.CPU.WriteMemInt(i, value);
+                }
+            }
+
+            CPUHelper.CPU.WriteEBP(newEbp);
+            CPUHelper.CPU.WriteESP(newEsp);
+        }
+    }
+}
