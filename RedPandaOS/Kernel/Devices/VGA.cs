88721645a86f97f9aaccad9bc07286053d8df828
giawa <giawa@hotmail.com>
2022-02-06 10:57:07 -0800

    [OS] Do not use VGA device unless it has been initialized (static constructor has been called)

--- RedPandaOS/Kernel/Devices/VGA.cs
+++ RedPandaOS/Kernel/Devices/VGA.cs
@@ -9,6 +9,7 @@ namespace Kernel.Devices
         private const int VGA_WIDTH = 80;
         private const int VGA_HEIGHT = 25;
         private static uint offset = VIDEO_MEMORY;
+        private static bool initialized = false;
 
         static VGA()
         {
@@ -34,6 +35,8 @@ namespace Kernel.Devices
 
         public static void WriteFormattedString(string s, uint u1, uint u2, uint u3, ushort effect = 0x0f00)
         {
+            if (!initialized) return;  // not initialized yet
+
             for (int i = 0; i < s.Length; i++)
             {
                 var c = s[i];
@@ -66,6 +69,8 @@ namespace Kernel.Devices
 
         public static void WriteInt(uint value)
         {
+            if (!initialized) return;  // not initialized yet
+
             uint divisor = 1;
 
             while (divisor * 10 <= value)
@@ -88,6 +93,8 @@ namespace Kernel.Devices
 
         public static void WriteInt(int value)
         {
+            if (!initialized) return;  // not initialized yet
+
             if (value < 0)
             {
                 VGA.WriteChar('-');
@@ -98,6 +105,8 @@ namespace Kernel.Devices
 
         public static void WriteString(string s, ushort effect = 0x0f00)
         {
+            if (!initialized) return;  // not initialized yet
+
             for (int i = 0; i < s.Length; i++)
             {
                 var c = s[i];
@@ -113,6 +122,8 @@ namespace Kernel.Devices
 
         public static void WriteLine()
         {
+            if (!initialized) return;  // not initialized yet
+
             offset += 160;
             offset -= (offset - VIDEO_MEMORY) % 160;
 
@@ -122,6 +133,8 @@ namespace Kernel.Devices
 
         public static void Delete()
         {
+            if (!initialized) return;  // not initialized yet
+
             offset -= 2;
 
             Scroll();
@@ -135,6 +148,7 @@ namespace Kernel.Devices
                 CPU.WriteMemory(i, (' ' | 0x0f00));
             }
             offset = VIDEO_MEMORY;
+            initialized = true;
 
             Scroll();
             SetCursorPos((offset - VIDEO_MEMORY) >> 1);
@@ -142,6 +156,8 @@ namespace Kernel.Devices
 
         public static void WriteChar(int c, ushort effect = 0x0f00)
         {
+            if (!initialized) return;  // not initialized yet
+
             CPU.WriteMemory((int)offset, (ushort)((c & 255) | effect));
             offset += 2;
             Scroll();
@@ -232,6 +248,8 @@ namespace Kernel.Devices
 
         public static void WriteHexChar(int value)
         {
+            if (!initialized) return;  // not initialized yet
+
             value &= 0x0f;
             if (value >= 10) WriteChar(value + 55);
             else WriteChar(value + 48);
