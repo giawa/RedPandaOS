576d3a4c0cf7cc229372c3b3e6e754e66a3914ee
giawa <giawa@hotmail.com>
2023-01-02 20:21:35 -0800

    [Emulator] Clean up usings

--- Emulator/CPU/x86/Registers.cs
+++ Emulator/CPU/x86/Registers.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Emulator.CPU.x86
+﻿namespace Emulator.CPU.x86
 {
     public partial class CPU
     {
