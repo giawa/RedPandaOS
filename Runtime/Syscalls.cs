88074a465b83714c662f994a0c6727c169bef0e5
giawa <giawa@hotmail.com>
2023-07-29 10:13:44 -0700

    [Runtime] Add specialized project for the runtime instead of having it built into RedPandaOS.dll

---
+++ Runtime/Syscalls.cs
@@ -0,0 +1,22 @@
+﻿using IL2Asm.BaseTypes;
+using System;
+
+namespace Runtime
+{
+    public static class Syscalls
+    {
+        [AsmMethod]
+        public static void WriteCharToStdOutSysCall(char c)
+        {
+            Console.Write(c);
+        }
+
+        [AsmPlug("Runtime.Syscalls.WriteCharToStdOutSysCall_Void_Char", IL2Asm.BaseTypes.Architecture.X86)]
+        public static void TempSyscallAsm(IAssembledMethod assembly)
+        {
+            assembly.AddAsm("pop eax");     // grab char
+            assembly.AddAsm("mov ebx, 1");  // WriteCharToStdOut
+            assembly.AddAsm("int 31");      // temporary print char interrupt
+        }
+    }
+}
