3f82c01c03377bcf4e46dea69efd01dd95601686
giawa <giawa@hotmail.com>
2023-08-11 19:56:21 -0700

    [Runtime] Move ObjectPool to Runtime library and rewrite to use a BitArray to keep track of available items

---
+++ Runtime/Collections/ObjectPool.cs
@@ -0,0 +1,45 @@
+﻿namespace Runtime.Collections
+{
+    public class ObjectPool<T> where T : class
+    {
+        private T[] _array;
+        private BitArray _inUse;
+
+        public ObjectPool(T[] array)
+        {
+            _array = array;
+            _inUse = new BitArray(Math32.Divide(array.Length, 32));
+        }
+
+        public bool TryAlloc(out T? item)
+        {
+            int index = _inUse.IndexOfFirstZero();
+
+            if (index >= 0)
+            {
+                _inUse[index] = true;
+                item = _array[index];
+                return true;
+            }
+            else
+            {
+                item = null;
+                return false;
+            }
+        }
+
+        public void Free(T item)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == item)
+                {
+                    _inUse[i] = false;
+                    return;
+                }
+            }
+
+            throw new System.Exception("Item did not exist in ObjectPool");
+        }
+    }
+}
